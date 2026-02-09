5a8b95ff2fde483fe623d4cbe78a2eed2b4e76b9
John Otele <Johnotele2@gmail.com>
2026-02-09 17:36:39 -0500

    Initial project structure with User and Task Classes

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+#include "User.h"
+#include "Task.h"
+
+using namespace std;
+
+int main(){
+    User user; 
+    Task task; 
+
+    cout << "Task Manager started successfully.\n";; 
+    return 0;
+}
